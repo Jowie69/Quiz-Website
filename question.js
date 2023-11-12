e6ceb7e9d7fb31f2de3dde1e87ea64b716c4c742
Jowie69 <eiwojserolf032002@gmail.com>
2023-11-12 12:54:12 +0800

    commited all changes

---
+++ question.js
@@ -0,0 +1,63 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Saan binaril si Jose Rizal?",
+        answer: "D. Sa Bagumbayan.",
+        options: [ 
+            "A. Sa likod.",
+            "B. Sa Rizal Park.",
+            "C. Sa 'di n'ya makita.",
+            "D. Sa Bagumbayan."
+        ]
+    },
+    {
+        numb: 2,
+        question: "Ano ang pangalan ng tanyag na makata at pambansang bayani ng Pilipinas na kilala sa kanyang mga tula at naging biktima ng eksekusyon noong Disyembre 30, 1896?",
+        answer: "A. Jose Rizal",
+        options: [
+            "A. Jose Rizal",
+            "B. Emilio Aguinaldo",
+            "C. Marcelo H. del Pilar",
+            "D. Andres Bonifacio"
+    
+        ]
+    },
+    {
+        numb: 3,
+        question: "Sa anong lugar idinaraos ang unang pagtatagpo nina Magellan at Lapu-Lapu, kung saan naganap ang Labanan sa Mactan noong 1521?",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+            "A. Palawan",
+            "B. Bohol",
+            "C. Boracay",
+            "D. Cebu"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Para saan ka bumabangon?",
+        answer: "B. Para sa grades.",
+        options: [
+            "A. Para sa'yo.",
+            "B. Para sa grades.",
+            "C. Para sa future ko.",
+            "D. Ayaw ko na lang sumagot."
+    
+        ]
+    },
+    {
+        numb: 5,
+        question: "Anong grade mo sa thesis pag di mo kinuhang developer si Jowie?",
+        answer: "D. 5",
+        options: [
+            "A. 1",
+            "B. 2",
+            "C. 3",
+            "D. 5"
+            
+
+    
+        ]
+    }
+
+];
